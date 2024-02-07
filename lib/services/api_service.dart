46afdfc76aa8d0a67c10756de2e66367faeeb0ee
pablichoka <pabloruiz16@hotmail.com>
2024-02-07 22:38:36 +0100

    Remove certificates and ssl connection for development

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
 
-import 'package:dio/dio.dart';
 import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 import 'package:kcal_control_frontend/models/user.dart';
@@ -15,54 +14,25 @@ class ApiService {
     return _singleton;
   }
 
-  final String _baseUrl = 'https://localhost:8081';
+  final String _baseUrl = 'http://localhost:8081';
   late String? _accessToken;
   late String? _refreshToken;
   late String? id;
   late String? _role;
 
-  Future<SecurityContext> get globalContext async {
-    final sslCert = await rootBundle.load('assets/certificates/localhost.pem');
-    SecurityContext securityContext = SecurityContext(withTrustedRoots: false);
-    securityContext.setTrustedCertificatesBytes(sslCert.buffer.asInt8List());
-    return securityContext;
-  }
-
   Future<bool> login(String username, String password) async {
     Uri uri = Uri.parse('$_baseUrl/login');
-    final request = await HttpClient(context: await globalContext)
-        .postUrl(uri)
-        .then((HttpClientRequest request) {
-      request.headers.contentType =
-          ContentType('application', 'json', charset: 'utf-8');
-      request.write(jsonEncode({'username': username, 'password': password}));
-      return request.close();
-    });
-    // final response = await http.post(uri,
-    //     headers: {'Content-Type': 'application/json; charset=UTF-8'},
-    //     body: json.encode({'username': username, 'password': password}));
-    // if (response.statusCode == 200) {
-    //   _accessToken = (jsonDecode(response.body)[
-    //       'accessToken']); // Should extract token from response body.
-    //   _refreshToken = (jsonDecode(response.body)['refreshToken']);
-    //   id = jsonDecode(response.body)['id'];
-    //   _role = jsonDecode(response.body)['roleName'];
-    //   return true;
-    // } else {
-    //   throw Exception('Failed to login: ${response.body}');
-    // }
-
-    final response = await request.transform(utf8.decoder).join();
-
-    final responseData = jsonDecode(response);
-    if (responseData['statusCode'] == 200) {
-      _accessToken = responseData['accessToken'];
-      _refreshToken = responseData['refreshToken'];
-      id = responseData['id'];
-      _role = responseData['roleName'];
+    final response = await http.post(uri,
+        headers: {'Content-Type': 'application/json; charset=UTF-8'},
+        body: json.encode({'username': username, 'password': password}));
+    if (response.statusCode == 200) {
+      _accessToken = (jsonDecode(response.body)['accessToken']);
+      _refreshToken = (jsonDecode(response.body)['refreshToken']);
+      id = jsonDecode(response.body)['id'];
+      _role = jsonDecode(response.body)['roleName'];
       return true;
     } else {
-      throw Exception('Failed to login: $response');
+      throw Exception('Failed to login: ${response.body}');
     }
   }
 
